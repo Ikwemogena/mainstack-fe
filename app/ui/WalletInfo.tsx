55f9bb7ed8b5c9632be7fe632ecc1d5137528f81
Ikwemogena Abdulai <abdulaimogena@gmail.com>
2024-04-23 20:45:39 +0100

    Add WalletInfo component for displaying wallet balances

---
+++ app/ui/WalletInfo.tsx
@@ -0,0 +1,87 @@
+'use client'
+import Image from "next/image";
+import { getWallet } from "../lib/actions";
+import type { Wallet } from "../lib/definitions";
+import { useEffect, useState } from "react";
+
+export function Wallet() {
+    const [balances, setBalances] = useState<Wallet>();
+
+    useEffect(() => {
+        getWallet().then((data) => {
+            setBalances(data);
+        });
+
+    }, [])
+    // const balances = await getWallet();
+    // console.log(balances)
+    return (
+        <div className="transactions-summary">
+            <div className="transactions-summary__item">
+                <div className="transactions-summary__item-title">
+                    <p>Ledger Balance</p>
+                    <div>
+                        <Image
+                            src="/assets/icons/info-icon.svg"
+                            alt="Mainstack Logo"
+                            width={20}
+                            height={20}
+                            priority
+                        />
+                    </div>
+                </div>
+                <p className="transactions-summary__item-value">USD {balances?.balance}</p>
+            </div>
+
+            <div className="transactions-summary__item">
+                <div className="transactions-summary__item-title">
+                    <p>Total Payout</p>
+                    <div>
+                        <Image
+                            src="/assets/icons/info-icon.svg"
+                            alt="Mainstack Logo"
+                            width={20}
+                            height={20}
+                            priority
+                        />
+                    </div>
+                </div>
+                <p className="transactions-summary__item-value">USD {balances?.total_payout}</p>
+            </div>
+
+            <div className="transactions-summary__item">
+                <div className="transactions-summary__item-title">
+                    <p>Total Revenue</p>
+                    <div>
+                        <Image
+                            src="/assets/icons/info-icon.svg"
+                            alt="Mainstack Logo"
+                            width={20}
+                            height={20}
+                            priority
+                        />
+                    </div>
+                </div>
+                <p className="transactions-summary__item-value">USD {balances?.total_revenue}</p>
+            </div>
+
+            <div className="transactions-summary__item">
+                <div>
+                    <div className="transactions-summary__item-title">
+                        <p>Pending Payout</p>
+                        <div>
+                            <Image
+                                src="/assets/icons/info-icon.svg"
+                                alt="Mainstack Logo"
+                                width={20}
+                                height={20}
+                                priority
+                            />
+                        </div>
+                    </div>
+                    <p className="transactions-summary__item-value">USD {balances?.pending_payout}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
